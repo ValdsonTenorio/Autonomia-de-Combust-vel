06a6a335a6db8b5fa3537908b75c0347f1886e11
Valdson <valdsonmartinstenorio721@gmail.com>
2024-11-24 14:30:31 -0400

    Criação view registro

---
+++ controlegasolina/lib/view/registro_view.dart
@@ -0,0 +1,97 @@
+import 'package:controlegasolina/controller/auth_controller.dart';
+import 'package:flutter/material.dart';
+
+class RegisterScreen extends StatefulWidget {
+  @override
+  _RegisterScreenState createState() => _RegisterScreenState();
+}
+
+class _RegisterScreenState extends State<RegisterScreen> {
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  final _nicknameController = TextEditingController();
+  String _errorMessage = '';
+  final AuthController _authController = AuthController();
+
+  Future<void> _register() async {
+    try {
+      await _authController.register(
+        _emailController.text,
+        _passwordController.text,
+        _nicknameController.text,
+      );
+      Navigator.pushReplacementNamed(context, '/home');
+    } catch (e) {
+      setState(() {
+        _errorMessage = 'Erro ao registrar: ${e.toString()}';
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Registrar'),
+        backgroundColor: Color(0xFF4A148C),
+        foregroundColor: Colors.white,
+      ),
+      body: Container(
+        color: Color(0xFF2E2E2E),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              _buildTextField(_nicknameController, 'Nickname'),
+              _buildTextField(_emailController, 'Email'),
+              _buildTextField(_passwordController, 'Senha', obscureText: true),
+              if (_errorMessage.isNotEmpty)
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 8.0),
+                  child: Text(
+                    _errorMessage,
+                    style: TextStyle(color: Colors.red),
+                  ),
+                ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: _register,
+                style: ElevatedButton.styleFrom(backgroundColor: Color(0xFF4A148C)),
+                child: Text('Registrar', style: TextStyle(color: Colors.white)),
+              ),
+              TextButton(
+                onPressed: () => Navigator.pushNamed(context, '/'),
+                child: Text('Já tem uma conta? Login', style: TextStyle(color: Colors.white)),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildTextField(TextEditingController controller, String label, {bool obscureText = false}) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: TextField(
+        controller: controller,
+        decoration: InputDecoration(
+          labelText: label,
+          labelStyle: TextStyle(color: Colors.white),
+          filled: true,
+          fillColor: Color(0xFF4A148C).withOpacity(0.2),
+          border: OutlineInputBorder(),
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.white),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Color(0xFF4A148C)),
+          ),
+        ),
+        style: TextStyle(color: Colors.white),
+        obscureText: obscureText,
+      ),
+    );
+  }
+}
