b5129e954d41606cb225b3a87dc2477c0a05e164
Valdson <valdsonmartinstenorio721@gmail.com>
2024-11-24 14:32:51 -0400

    Criação main e atualizações

--- controlegasolina/lib/main.dart
+++ controlegasolina/lib/main.dart
@@ -1,73 +1,42 @@
-import 'package:controlegasolina/controller/auth_controller.dart';
+import 'package:controlegasolina/view/adicionar_veiculo.dart';
+import 'package:controlegasolina/view/auth_view.dart';
+import 'package:controlegasolina/view/historico_abastecimento.dart';
+import 'package:controlegasolina/view/home_view.dart';
+import 'package:controlegasolina/view/meus_veiculos.dart';
+import 'package:controlegasolina/view/perfil_view.dart';
+import 'package:controlegasolina/view/registro_view.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 
-class MyDrawer extends StatelessWidget {
-  final AuthController _authController = AuthController();
 
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  print("Inicializando Firebase...");
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  print("Firebase inicializado com sucesso.");
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    User? user = _authController.getCurrentUser();
-
-    return Drawer(
-      child: ListView(
-        padding: EdgeInsets.zero,
-        children: [
-          UserAccountsDrawerHeader(
-            accountName: Text(user?.displayName ?? 'Nome do Usuário', style: TextStyle(color: Colors.white)),
-            accountEmail: Text(user?.email ?? 'email@exemplo.com', style: TextStyle(color: Colors.white)),
-            decoration: BoxDecoration(
-              color: Color(0xFF4A148C),
-            ),
-          ),
-          ListTile(
-            leading: Icon(Icons.home),
-            iconColor: Color(0xFF4A148C),
-            title: Text('Home'),
-            textColor: Colors.white,
-            onTap: () => Navigator.pushNamed(context, '/home'),
-          ),
-          ListTile(
-            leading: Icon(Icons.car_rental),
-            iconColor: Color(0xFF4A148C),
-            title: Text('Meus Veículos'),
-            textColor: Colors.white,
-            onTap: () => Navigator.pushNamed(context, '/meus_veiculos'),
-          ),
-          ListTile(
-            leading: Icon(Icons.add),
-            iconColor: Color(0xFF4A148C),
-            title: Text('Adicionar Veículo'),
-            textColor: Colors.white,
-            onTap: () => Navigator.pushNamed(context, '/adicionar_veiculo'),
-          ),
-          ListTile(
-            leading: Icon(Icons.history),
-            iconColor: Color(0xFF4A148C),
-            title: Text('Histórico de Abastecimentos'),
-            textColor: Colors.white,
-            onTap: () => Navigator.pushNamed(context, '/historico_abastecimentos'),
-          ),
-          ListTile(
-            leading: Icon(Icons.person),
-            iconColor: Color(0xFF4A148C),
-            title: Text('Perfil'),
-            textColor: Colors.white,
-            onTap: () => Navigator.pushNamed(context, '/perfil'),
-          ),
-          ListTile(
-            leading: Icon(Icons.logout),
-            iconColor: Color.fromARGB(255, 209, 8, 8),
-            title: Text('Logout'),
-            textColor: Colors.white,
-            onTap: () async {
-              await _authController.logout();
-              Navigator.pushReplacementNamed(context, '/');
-            },
-          ),
-        ],
+    return MaterialApp(
+      title: 'Controle de Abastecimento',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
-      backgroundColor: Color(0xFF2E2E2E),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => LoginScreen(),
+        '/home': (context) => HomeScreen(),
+        '/meus_veiculos': (context) => MeusVeiculosScreen(),
+        '/adicionar_veiculo': (context) => AdicionarVeiculoScreen(),
+        '/historico_abastecimentos': (context) => HistoricoAbastecimentosScreen(),
+        '/perfil': (context) => PerfilScreen(),
+        '/register': (context) => RegisterScreen()
+      },
     );
   }
 }
